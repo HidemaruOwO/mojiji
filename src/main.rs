8db571c2a38885115fd9b0dae4d9fa4a3bf65ed2
HidemaruOwO <owner@v-sli.me>
2025-03-18 01:27:37 +0900

    ✨ add simple web api (src/main.rs)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{http::ContentType, response::content};
+use text_image::{image::dynamic_image_to_vec, process::process};
+
+mod text_image;
+
+#[macro_use]
+extern crate rocket;
+
+#[derive(Responder)]
+#[response(content_type = "image/png")]
+struct Image(Vec<u8>);
+
+#[get("/")]
+fn index() -> &'static str {
+    "mojiji is running now."
+}
+
+#[get("/generate")]
+fn generate() -> Result<Image, &'static str> {
+    let image = match process("おはようございます", "Noto") {
+        Ok(v) => v,
+        Err(e) => return Err(e),
+    };
+
+    let png = dynamic_image_to_vec(&image, image::ImageOutputFormat::Png)
+        .expect("Failed to generate png image.");
+
+    Ok(Image(png))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, generate])
 }
